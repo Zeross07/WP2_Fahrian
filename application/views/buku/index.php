5386fedf84fcadf89315fdeacafc93cdf11dd744
Zeross07 <fahrian1108@gmail.com>
2024-06-02 19:18:36 +0700

    first commit

--- application/views/buku/index.php
+++ application/views/buku/index.php
@@ -83,7 +83,7 @@
                         <select name="id_kategori" class="form-control form-control-user">
                             <option value="">Pilih Kategori</option>
                             <?php foreach ($kategori as $k): ?>
-                            <option value="<?= $k['id'];?>"><?= $k['kategori'];?></option>
+                            <option value="<?= $k['id_kategori'];?>"><?= $k['nama_kategori'];?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
